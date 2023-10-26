73519b8e91c53c1716977746307665d793e23a60
Jeeveeka1726 <141898782+Jeeveeka1726@users.noreply.github.com>
2023-10-26 10:42:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,141 @@
+var cartValue = document.getElementById("cart-value");
+var cartButton = document.getElementById("cart");
+
+var addButtons = document.getElementsByClassName("button");
+
+var items = [
+  {
+    name: "This was our pact",
+    quantity: 0,
+    dollars: 7,
+    cents: 49,
+  },
+  {
+    name: "The famous five",
+    quantity: 0,
+    dollars: 4,
+    cents: 59,
+  },
+  {
+    name: "Matilda",
+    quantity: 0,
+    dollars: 6,
+    cents: 80,
+  },
+  {
+    name: "Harry Potter",
+    quantity: 0,
+    dollars: 10,
+    cents: 0,
+  },
+  {
+    name: "For Young Readers",
+    quantity: 0,
+    dollars: 7,
+    cents: 29,
+  },
+  {
+    name: "Wimpy Kid - DIY",
+    quantity: 0,
+    dollars: 4,
+    cents: 99,
+  },
+  {
+    name: "Dart Board",
+    quantity: 0,
+    dollars: 17,
+    cents: 49,
+  },
+  {
+    name: "Connect Four",
+    quantity: 0,
+    dollars: 19,
+    cents: 99,
+  },
+  {
+    name: "Jenga",
+    quantity: 0,
+    dollars: 20,
+    cents: 99,
+  },
+  {
+    name: "Monopoly",
+    quantity: 0,
+    dollars: 19,
+    cents: 49,
+  },
+  {
+    name: "Bookmarks",
+    quantity: 0,
+    dollars: 12,
+    cents: 49,
+  },
+  {
+    name: "Birthday Card",
+    quantity: 0,
+    dollars: 12,
+    cents: 49,
+  },
+  {
+    name: "Stuffed toys",
+    quantity: 0,
+    dollars: 15,
+    cents: 99,
+  },
+  {
+    name: "Dream catcher drawing",
+    quantity: 0,
+    dollars: 18,
+    cents: 49,
+  },
+];
+
+function updateCart() {
+  let cart = 0;
+  for (index = 0; index < items.length; index++) {
+    cart = cart + items[index].quantity;
+  }
+  cartValue.innerHTML = cart;
+}
+
+for (let i = 0; i < addButtons.length; i++) {
+  addButtons[i].onclick = () => {
+    items[i].quantity++;
+    updateCart();
+  };
+}
+
+var finalDollars = 0;
+var finalCents = 0;
+
+function updatePrice() {
+  let totalPriceInCents = 0;
+
+  for (index = 0; index < items.length; index++) {
+    totalPriceInCents =
+      totalPriceInCents +
+      items[index].quantity * (items[index].dollars * 100 + items[index].cents);
+  }
+  finalDollars = Math.floor(totalPriceInCents / 100);
+  finalCents = totalPriceInCents % 100;
+}
+
+
+
+var Wlink ="https://api.whatsapp.com/send?phone=919000000000&text=Order%20details";
+
+function updateLink() {
+  for (let i = 0; i< items.length; i++) {
+    if (items[i].quantity != 0) {
+      Wlink += "%0A" + items[i].name + "%20" + items[i].quantity;
+    }
+  }
+  Wlink +="Total%20Price:%20$" + finalDollars + "%20" + finalCents + "cents";
+}
+
+cartButton.onclick = () => {
+  updatePrice();
+  updateLink();
+  window.open(Wlink, "_blank");
+
+};
